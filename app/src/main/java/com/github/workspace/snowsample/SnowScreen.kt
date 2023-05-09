abf5882267b0bbeaab70f51a36582abeb5357915
Kimin Ryu <ryu@holix.com>
2023-05-09 22:03:46 +0900

    page 18

--- app/src/main/java/com/github/workspace/snowsample/SnowScreen.kt
+++ app/src/main/java/com/github/workspace/snowsample/SnowScreen.kt
@@ -6,10 +6,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
 
 @Composable
 fun SnowScreen() {
+    val paint: Paint = Paint().apply {
+        isAntiAlias = true
+        color = Color.White
+        style = PaintingStyle.Fill
+    }
     Canvas(modifier = Modifier.fillMaxSize().background(Color.Black)) {
-        drawCircle(color = Color.White, radius = 20F)
+        val canvas = drawContext.canvas
+        canvas.drawCircle(center, 20f, paint)
+//        drawCircle(color = Color.White, radius = 20F)
     }
 }
