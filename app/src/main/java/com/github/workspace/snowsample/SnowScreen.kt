7c6b0eccf01d2b80b0e7606dfaad4203e2914f15
Kimin Ryu <ryu@holix.com>
2023-05-09 21:55:40 +0900

    init

---
+++ app/src/main/java/com/github/workspace/snowsample/SnowScreen.kt
@@ -0,0 +1,15 @@
+package com.github.workspace.snowsample
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun SnowScreen() {
+    Canvas(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+
+    }
+}
